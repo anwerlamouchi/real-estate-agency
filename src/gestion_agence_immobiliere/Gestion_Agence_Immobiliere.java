2d686c0a58855d1786f8b36f061b348257c2c31f
jemacom <jemacomchokri@gmail.com>
2014-10-22 23:00:00 +0100

    First commit Real estate agency

---
+++ src/gestion_agence_immobiliere/Gestion_Agence_Immobiliere.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gestion_agence_immobiliere;
+
+/**
+ *
+ * @author jemacom
+ */
+public class Gestion_Agence_Immobiliere {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Gestion_offres g_offre = new Gestion_offres();
+        g_offre.show();
+    }
+    
+}
